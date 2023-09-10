5b98b120ec65b4550b3fa38d7789fcfe07806fc3
kpever <2584586534@qq.com>
2023-09-10 09:51:49 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/coolweather/db/Province.java
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -3,9 +3,13 @@ package com.example.coolweather.db;
 import org.litepal.crud.DataSupport;
 
 public class Province extends DataSupport {
+
     private int id;
+
     private String provinceName;
+
     private int provinceCode;
+
     public int getId() {
         return id;
     }
